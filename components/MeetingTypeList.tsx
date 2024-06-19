dd43cd72162d1926d6f84a59f5b08fc5aa49d54e
Vishu Bansal <bansalvishu56@gmail.com>
2024-06-19 22:59:45 +0530

    meeting model complete

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -4,12 +4,16 @@ import React, { useState } from "react";
 import HomeCard from "./HomeCard";
 import { useRouter } from "next/navigation";
 import { set } from "mongoose";
+import MeetingModel from "./MeetingModel";
 
 const MeetingTypeList = () => {
   const router = useRouter();
   const [meetingState, setMeetingState] = useState<
     "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
   >();
+
+  const createMeeting = () => {};
+
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
       <HomeCard
@@ -17,7 +21,7 @@ const MeetingTypeList = () => {
         title="New Meeting"
         description="Start an instant meeting"
         handleClick={() => {
-          setMeetingState("isJoiningMeeting");
+          setMeetingState("isInstantMeeting");
         }}
         className="bg-orange-1"
       />
@@ -47,6 +51,17 @@ const MeetingTypeList = () => {
           setMeetingState("isJoiningMeeting");
         }}
         className="bg-yellow-1"
+      />
+
+      <MeetingModel
+        isOpen={meetingState === "isInstantMeeting"}
+        onClose={() => {
+          setMeetingState(undefined);
+        }}
+        title="Start an Instant Meeting"
+        className="text-center"
+        buttonText="Start Meeting"
+        handleClick={createMeeting}
       />
     </section>
   );
